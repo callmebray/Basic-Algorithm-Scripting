cf7071f1e99e3a79d21cd3d31a7cdd55e9581134
Brady Ashby <brady.ashby4@gmail.com>
2017-04-18 17:28:07 -0600

    Update projects.js

--- projects.js
+++ projects.js
@@ -39,3 +39,16 @@ function largestOfFour(arr) {
 }
 
 largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+/*Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.*/
+function confirmEnding(str, target) {
+  
+   if (str.substr(str.length - target.length) == target) {
+     return true;
+   }else return false;
+
+}
+
+confirmEnding("Bastian", "n");
