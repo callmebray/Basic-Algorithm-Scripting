01077a16516fdfe071cf162df4a253d85d4788bd
Brady Ashby <brady.ashby4@gmail.com>
2017-04-18 22:42:35 -0600

    Update projects.js

--- projects.js
+++ projects.js
@@ -105,3 +105,15 @@ function chunkArrayInGroups(arr, size) {
 }
 
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);
+
+/*Return the remaining elements of an array after chopping off n elements from the head.*/
+
+function slasher(arr, howMany) {
+  
+  arr.splice(0,howMany);
+  
+  return arr;
+
+}
+
+slasher([1, 2, 3], 2);
