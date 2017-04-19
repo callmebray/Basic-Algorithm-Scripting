adc3340b67028f89557a0a396c0f420b4bf2e139
Brady Ashby <brady.ashby4@gmail.com>
2017-04-18 21:50:00 -0600

    Update projects.js

--- projects.js
+++ projects.js
@@ -72,3 +72,17 @@ function repeatStringNumTimes(str, num) {
 }
 
 repeatStringNumTimes("abc", 3);
+
+/*Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.*/
+function truncateString(str, num) {
+  
+  if (str.length > num && num > 2) {
+    return str.slice(0, num - 3) + '...';
+  } 
+  else if (str.length > num && num > 0) {
+    return str.slice(0, num) + '...';
+  } 
+  else return str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
