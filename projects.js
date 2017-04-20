85c5db3b983d4d23525aa4ae3507b15fe11d5e87
Brady Ashby <brady.ashby4@gmail.com>
2017-04-20 15:53:43 -0600

    Update projects.js

--- projects.js
+++ projects.js
@@ -147,3 +147,13 @@ function bouncer(arr) {
 }
 
 bouncer([7, "ate", "", false, 9]);
+
+/*Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.*/
+function getIndexToIns(arr, num) {
+  
+  arr.push(num);
+  arr.sort(function(a,b){return a-b;});
+  return arr.indexOf(num);
+}
+
+getIndexToIns([3, 10, 5], 3);
